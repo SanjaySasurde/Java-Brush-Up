0e983738a001002663e09067c6c3dd1b9893becf
SanjaySasurde <sanjaysasurde07@gmail.com>
2024-10-23 15:51:12 +0530

    new file added

---
+++ TreeSetDemo11.java
@@ -0,0 +1,10 @@
+import java.util.TreeSet;
+
+public class TreeSetDemo11 {
+    public static void main(String[] args) {
+        TreeSet t=new TreeSet();
+        t.add(1);
+        t.add(2);
+        System.out.println(t);
+    }
+}
